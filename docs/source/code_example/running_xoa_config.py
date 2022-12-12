6a95dac35682a3a7a17302830a02bb985bb68691
Leonard Yu <hyu@xenanetworks.com>
2022-12-12 12:55:08 +0100

    docs: Udpate two code examples

--- docs/source/code_example/running_xoa_config.py
+++ docs/source/code_example/running_xoa_config.py
@@ -12,11 +12,20 @@ PLUGINS_PATH = BASE_PATH / "pluginlib"
 TEST_CONFIG_PATH = BASE_PATH / "my2544_data.json"
 
 
-async def do_test(tester: types.Credentials, suite_name, data):
+async def do_test(tester: types.Credentials, suite_name: str, config):
+    # Create a default instance of the controller class.
     ctrl = await controller.MainController()
+
+    # Register the plugins folder.
     ctrl.register_lib(PLUGINS_PATH)
+
+    # Add tester credentials into teh controller. If already added, it will be ignored.
+    # If you want to add a list of testers, you need to iterate through the list.
     await ctrl.add_tester(tester)
-    execution_id = ctrl.start_test_suite(suite_name, data)
+
+    # Test suite name: "RFC-2544" is received from call of c.get_available_test_suites()
+    execution_id = ctrl.start_test_suite(suite_name, config)
+    
     async for msg in ctrl.listen_changes(execution_id, _filter={types.EMsgType.STATISTICS}):
         print(msg)
 
@@ -29,8 +38,8 @@ async def main() -> None:
     )
     # Load your test configuration data into the test suite and run.
     with open(TEST_CONFIG_PATH, "r") as f:
-        data = json.load(f)
-        await do_test(my_tester_credential, "RFC-2544", data)
+        config = json.load(f)
+        await do_test(my_tester_credential, "RFC-2544", config)
 
 
 if __name__ == "__main__":
