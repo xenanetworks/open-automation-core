10bca312f8cfff86bde7dc0fd32e0d15dfe3a559
ArtemConstantinov <y_4ox@yahoo.com>
2023-05-17 16:19:26 +0800

    Expose exceptions

--- xoa_core/exceptions.py
+++ xoa_core/exceptions.py
@@ -6,8 +6,19 @@ from .core.exceptions import (
     MultiModeError,
     TestSuiteNotExistError,
 )
-
+from .core.resources.resource.exceptions import (
+    InvalidTesterTypeError,
+    TesterCommunicationError,
+    IsDisconnectedError,
+    IsConnectedError,
+    UnknownResourceError,
+)
 __all__ = (
     "MultiModeError",
     "TestSuiteNotExistError",
+    "InvalidTesterTypeError",
+    "TesterCommunicationError",
+    "IsDisconnectedError",
+    "IsConnectedError",
+    "UnknownResourceError",
 )
